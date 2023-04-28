18463910dd24286f68fc9fee7c9f4843b3327991
yucemetin <red.banana988@gmail.com>
2023-04-29 02:03:03 +0300

    functions 2

--- Functions/main.go
+++ Functions/main.go
@@ -6,6 +6,10 @@ func add(num1 int, num2 int) int {
 	return num1 + num2
 }
 
+func add2(num1, num2 int) int { // eğer parametredeki değerlerin hepsi aynı tür ise tek bir int anahtarı yeterlidir.
+	return num1 + num2
+}
+
 func function(num int) (x, y int) { // parantez içinde return edilecek değişkenler tanımlanabilir.
 	x = num / 2
 	y = num * 2
@@ -26,7 +30,7 @@ func main() {
 	num4 := 20
 
 	fmt.Println(add(num1, num2))
-	fmt.Println(add(num3, num4))
+	fmt.Println(add2(num3, num4))
 
 	fmt.Println(function(10))
 	fmt.Println(function2(10))
